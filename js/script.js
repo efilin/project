98d47c18d379159dfafa2dfac699b22e4c44450c
Eygeniy <example@gmail.com>
2021-11-04 11:04:16 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+//console.log(something);
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?","18");
+//console.log(typeof(answer));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
